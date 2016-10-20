e3a0e9b7ee7e501aada95380bb0b1d0c060ca647
nextitsoftsys <yadav.jayprakash20@yahoo.in>
2016-10-20 10:22:52 +0530

    updated

--- server.js
+++ server.js
@@ -3,54 +3,88 @@ var cheerio = require('cheerio');
 var request = require('request');
 var bodyParser = require('body-parser');
 var app = express();
+var searchLink = [];
 var wordOfDay = [];
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.get('/a', function (req, res) {
+app.post('/game', function (req, res) {
     // allow access from other domains
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    var cat;
-    // use Cheerio to make request
+    var url = req.body.url;
+
+    // use Cheerio to make request for play Store search
     request({
         method: 'GET',
-        url: 'https://play.google.com/store/apps/details?id=dk.tactile.discoducks&hl=en'
+        url: 'https://play.google.com/store/search?q=' + url
+
     }, function (err, response, html, callback) {
+
         if (err) return console.error(err);
 
-        // get the HTML body from WordThink.com
+        // get the HTML body from google
         $ = cheerio.load(html);
+        var href;
+        $('a.card-click-target').each(function () {
+            var a = $(this);
+            href = a.attr('href');
+            if (href && href.indexOf('/store/apps/details?id=') != -1) {
+                searchLink.push({ key: href });
 
-        var main = $('.main-content')
-        // getting game title class
-        var t = main.find('.document-title');
-        var info = main.find('.meta-info ');
-        //getting game info
-        var title = t.find('.id-app-title').text();
-        //getting game info class
-        var pubdata = info.find('[itemprop=datePublished]').text();
-        var size = info.find('[itemprop=fileSize]').text();
-        var des = main.find('[jsname=C4s9Ed]').text();
-        $('a.document-subtitle.category').filter(function () {
+            }
 
-            var data = $(this);
-            cat = data.children().first().text();
 
         })
-        // create an object
-        wordOfDay.push({ gameTitle: title, Gametype: cat, datePublished: pubdata, fileSize: size, Info: des })
+        var finalserchlinks = 'https://play.google.com' + searchLink[0].key;
+        console.log('searchLink - ', finalserchlinks);
+        // use Cheerio to make request for game details
+        var cat;
+
+        request({
+            method: 'GET',
+            url: finalserchlinks
+        }, function (err, response, html, callback) {
+            if (err) return console.error(err);
+
+            // get the HTML body from WordThink.com
+            $ = cheerio.load(html);
+
+            var main = $('.main-content')
+            // getting game title class
+            var t = main.find('.document-title');
+            var info = main.find('.meta-info ');
+            //getting game info
+            var title = t.find('.id-app-title').text();
+            //getting game info class
+            var pubdata = info.find('[itemprop=datePublished]').text();
+            var size = info.find('[itemprop=fileSize]').text();
+            var des = main.find('[jsname=C4s9Ed]').text();
+            $('a.document-subtitle.category').filter(function () {
+
+                var data = $(this);
+                cat = data.children().first().text();
+
+            })
+            // create an object
+            wordOfDay.push({ gameTitle: title, Gametype: cat, datePublished: pubdata, fileSize: size, Info: des })
+            console.log('Data', wordOfDay);
+            res.send(JSON.stringify(wordOfDay));
+        });
+
+        // return a JSON object as a response
+
+        console.log('Data', wordOfDay);
 
     });
 
-    // return a JSON object as a response
-    res.send(JSON.stringify(wordOfDay));
-    console.log('Data', wordOfDay);
+
+
 });
 
 // start app on localhost port 3000
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 app.listen(port, function () {
     console.log('listening on port ' + port);
 });
