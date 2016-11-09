ea4b778e68b75c0d42467a90833405cce59cddbf
chandankrishnan <chandan.mandre10@gmail.com>
2016-11-09 17:25:11 +0530

    init

--- client/controller/searchCtrl.js
+++ client/controller/searchCtrl.js
@@ -1,51 +1,29 @@
 angular.module('MyApp')
-
     .controller('searchCtrl', function ($scope, $http, Upload, toastr) {
         $scope.find = function () {
-            var gameName = { 'url': this.name };
-
-
-            //sending game name to server
-            // $http.post('http://localhost:3006/getAppDetails', gameName).success(function (data) {
-            //     if (data) {
-            //         console.log(data)
-            //         var as = []
-            //         as.push(data)
-            //         $scope.data = as;
-
-            //     }
-            //     else {
-            //         toastr.error('error');
-            //     }
-
-            // })
-            
-              //sending package name to server
-             $http.post('http://localhost:3006/getCatagoryType', gameName).success(function (data) {
+            var gameName = {
+                'url': this.name
+            };
+            // sending game name to server
+            $http.post('http://localhost:3009/getAppDetails', gameName).success(function (data) {
                 if (data) {
-                    console.log(data[0])
+                    console.log(data)
                     var as = []
-                    as.push(data[0])
+                    as.push(data)
                     $scope.data = as;
-
-                }
-                else {
+                } else {
                     toastr.error('error');
                 }
-
             })
-
         }
 
         $scope.uploadFiles = function (file) {
-
-            console.log(file);
             if (window.FileReader) {
-                // FileReader are supported.
                 getAsText(file);
             } else {
                 toastr.error('FileReader are not supported in this browser.');
             }
+
             function getAsText(fileToRead) {
                 var reader = new FileReader();
                 // Read file into memory as UTF-8      
@@ -61,58 +39,50 @@ angular.module('MyApp')
             }
 
             function processData(csv) {
-                var allTextLines = csv.split(/\r\n|\n/);
-                var s = []
-                var tarr = [];
+                console.log("csv ", csv);
+                var allTextLines = csv.split(/\r\n|\n/),
+                    s = [],
+                    tarr = [];
                 for (var i = 1; i < allTextLines.length; i++) {
                     tarr.push(allTextLines[i])
                 }
 
-  /* remove comment and use  when u reading file which containes game name not package name
-  */
-                //     var h = tarr.toString();
-                //     var s = h.split(',')
-                //     //  console.log('dfsd',s)
-                //     var ka = [];
-                //     var p = 0;
-                //     for (p = 6; p < s.length; p++) {
-
-                //         ka.push(s[p]);
-                //       p++;p++;p++;p++;
-
-
-                //     }
-                // console.log('gh',ka)
-                var as = []
-                var d = new Date();
-                var stime = 0;
-                var etime = 0;
-                var ttime = 0;
+                /* remove comment and use  when u reading file which containes game name not package name
+                 */
+                var h = tarr.toString(),
+                    s = h.split(','),
+                    ka = [],
+                    p = 0;
+                for (p = 6; p < s.length; p++) {
+                    ka.push(s[p]);
+                    p++;
+                    p++;
+                    p++;
+                    p++;
+                }
+                var as = [],
+                    d = new Date(),
+                    stime = 0,
+                    etime = 0,
+                    ttime = 0;
                 for (var i = 1; i < tarr.length - 1; i++) {
 
-                    var gameName = { 'url': tarr[i] };
+                    var gameName = {
+                        'url': tarr[i]
+                    };
                     console.log(gameName)
-
                     stime = stime + d.getMilliseconds();
                     console.log('start time', stime);
                     //   sending game name to server
-
-                    $http.post('http://localhost:3006/getAppDetails', gameName).success(function (data) {
+                    $http.post('http://localhost:3009/getAppDetails', gameName).success(function (data) {
                         if (data) {
-
                             as.push(data)
-
- 
-                        }
-                        else {
+                        } else {
                             toastr.error('error');
                         }
-
                     })
-                    etime = etime + d.getMilliseconds();
-
+                    etime = etime - d.getMilliseconds();
                     console.log('end time', etime)
-
                 }
                 ttime = etime - stime;
                 console.log('tota  time', ttime);
@@ -126,12 +96,5 @@ angular.module('MyApp')
                     toastr.error("Canno't read file !");
                 }
             }
-
-
-
-
-
         }
-
-
     })
--- server/server.js
+++ server/server.js
@@ -1,107 +1,69 @@
-var express = require('express');
-var path = require('path');
-var fs = require("fs");
-var cheerio = require('cheerio');
-var request = require('request');
-var bodyParser = require('body-parser');
-var redis = require('redis');
-var Promise = require("bluebird");
+/**
+ * define require module
+ */
+var express = require('express'),
+    path = require('path'),
+    fs = require("fs"),
+    cheerio = require('cheerio'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    redis = require('redis'),
+    Promise = require("bluebird"),
+    db = redis.createClient(),
+    app = express(),
+    port = process.env.PORT || 3009,
+    firebase = require('./firbase.js');
+
 Promise.promisifyAll(require("redis"));
-var db = redis.createClient();
 
-var app = express();
-
-//config--
+/**
+ * configure app
+ */
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static(path.join(__dirname, '../client')));
-firebase = require('./firbase.js');
 
-//post call
+/*
+ * in this function getAppDetails
+ * @param {String} --url
+ * @return {String} --final
+ */
 app.post('/getAppDetails', function (req, res) {
-    var url = req.body.url;
-    console.log(url)
-
-    var hash = 5387;
+    var url = req.body.url,
+        hash = 5387;
     for (i = 0; i < url.length; i++) {
         char = url.charCodeAt(i);
-        hash = ((hash << 5) + hash) + char; /* hash * 33 + c */
+        hash = ((hash << 5) + hash) + char;
     }
-    //Retriving last four digits 
-    var hashkey = hash.toString();
-    var finalhkey = hashkey.substr(hashkey.length - 3, hashkey.length)
-    console.log(finalhkey, 'j', hash)
-  
-    
-    /*
-             when u search by using game name not package  remove comments then
-    */
-    
-    // use Cheerio to make request for play Store search
-    //     request({
-    //         method: 'GET',
-    //         url: 'https://play.google.com/store/search?q=' + url
-
-    //     }, function (err, response, html, callback) {
-
-    //         if (err) return console.error(err);
-
-    //         // get the HTML body from google
-    //         $ = cheerio.load(html);
-    //         var href;
-    //         var searchLink = [];
-    //         $('a.card-click-target').each(function () {
-    //             var a = $(this);
-    //             href = a.attr('href');
-    //             if (href && href.indexOf('/store/apps/details?id=') != -1) {
-    //                 searchLink.push({ key: href });
-
-    //             }
-    // })
-    //       console.log(searchLink[0])
-
-    //game link for seraching game detail
-    var finalserchlinks = 'https://play.google.com/store/apps/details?id=' + url;
+    var hashkey = hash.toString(),
+        finalhkey = hashkey.substr(hashkey.length - 3, hashkey.length),
+        cat,
+        finalserchlinks = 'https://play.google.com/store/apps/details?id=' + url;
     console.log('searchLink - ', finalserchlinks);
-    // use Cheerio to make request for game details
-    var cat;
-
-    request({  // 
+    request({
         method: 'GET',
         url: finalserchlinks
     }, function (err, response, html, callback) {
         if (err) return console.error(err);
-
-        // get the HTML body from google.com
         $ = cheerio.load(html);
-        var wordOfDay = [];
-        var main = $('.main-content')
-        // getting game title class
-        var t = main.find('.document-title');
-        var info = main.find('.meta-info ');
-        //getting game info
-        var title = t.find('.id-app-title').text();
-        //getting game info class
-        var pubdata = info.find('[itemprop=datePublished]').text();
-        var size = info.find('[itemprop=fileSize]').text();
-        // var des = main.find('[jsname=C4s9Ed]').text();
-        // var p=  $('[jsname=C4s9Ed]')
-        // var des=p.find('p').text();
-        var des = [];
+        var wordOfDay = [],
+            main = $('.main-content'),
+            t = main.find('.document-title'),
+            info = main.find('.meta-info '),
+            title = t.find('.id-app-title').text(),
+            pubdata = info.find('[itemprop=datePublished]').text(),
+            size = info.find('[itemprop=fileSize]').text(),
+            des = [];
         $('p').each(function (i, elem) {
             des[i] = $(this).text();
-
         });
         console.log('description', des[0]);
-
-
         $('a.document-subtitle.category').filter(function () {
-
             var data = $(this);
             cat = data.children().first().text();
-
         })
-        // create an object
         wordOfDay.push({
             gameTitle: title,
             Gametype: cat,
@@ -109,17 +71,9 @@ app.post('/getAppDetails', function (req, res) {
             fileSize: size,
             description: des[0],
         })
-
-
-
-        // Storing  data to redish
         db.hset([finalhkey, url, cat], redis.print);
-
-
-
-
         if (!des[0] == '') {
-            // storing game details to firbase
+            /* storing game details to firbase */
             var gameRef = firebase.database().ref("Game/gameDetails");
             gameRef.once('value', function (snapshot) {
                 if (!snapshot.hasChild(finalhkey)) {
@@ -134,17 +88,13 @@ app.post('/getAppDetails', function (req, res) {
                         package: hash,
                         package_name: url
                     });
-
-                    // Retriving data from firebase
+                    /* Retriving data from firebase */
                     gameRef.orderByChild('key').equalTo(finalhkey).on('child_added', function (data) {
                         var d = data.val();
                         res.send(d);
                         console.log("info", d);
                     });
-
-                }
-
-                else {
+                } else {
                     console.log("That Game name already exists");
                     var d = snapshot.val();
                     console.log("info", d[finalhkey]);
@@ -153,121 +103,83 @@ app.post('/getAppDetails', function (req, res) {
 
             });
 
-        }
-        else {
+        } else {
             console.log('Description Not prasent')
         }
-
-
     });
-    // });
 });
 
-//post call for gametype
+/*
+ * in this search by gametype
+ * @param {String} --url
+ * @return {String} --final
+ */
 app.post('/getCatagoryType', function (req, res) {
-    var url = req.body.url;
-    console.log(url)
-
-    var hash = 5387;
+    var url = req.body.url,
+        hash = 5387;
     for (i = 0; i < url.length; i++) {
         char = url.charCodeAt(i);
-        hash = ((hash << 5) + hash) + char; /* hash * 33 + c */
+        hash = ((hash << 5) + hash) + char;
     }
-    //Retriving last four digits 
-    var hashkey = hash.toString();
-    var finalhkey = hashkey.substr(hashkey.length - 3, hashkey.length)
-    console.log(finalhkey, 'j', hash)
+    var hashkey = hash.toString(),
+        finalhkey = hashkey.substr(hashkey.length - 3, hashkey.length),
+        finalserchlinks = 'https://play.google.com/store/apps/details?id=' + url,
+        cat;
 
-    // use Cheerio to make request for play Store search for game name
-    //     request({
-    //         method: 'GET',
-    //         url: 'https://play.google.com/store/search?q=' + url
-
-    //     }, function (err, response, html, callback) {
-
-    //         if (err) return console.error(err);
-
-    //         // get the HTML body from google
-    //         $ = cheerio.load(html);
-    //         var href;
-    //         var searchLink = [];
-    //         $('a.card-click-target').each(function () {
-    //             var a = $(this);
-    //             href = a.attr('href');
-    //             if (href && href.indexOf('/store/apps/details?id=') != -1) {
-    //                 searchLink.push({ key: href });
-
-    //             }
-    // })
-    //       console.log(searchLink[0])
-
-    //game link for seraching game detail
-    var finalserchlinks = 'https://play.google.com/store/apps/details?id=' + url;
-    console.log('searchLink - ', finalserchlinks);
-    // use Cheerio to make request for game details
-    var cat;
-
-    request({  // 
+    request({
         method: 'GET',
         url: finalserchlinks
     }, function (err, response, html, callback) {
         if (err) return console.error(err);
 
-        // get the HTML body from google.com
+        /* get the HTML body from google.com */
         $ = cheerio.load(html);
-        var wordOfDay = [];
-        var main = $('.main-content')
-        // getting game title class
-        var t = main.find('.document-title');
-        var info = main.find('.meta-info ');
-        //getting game info
-        var title = t.find('.id-app-title').text();
-        //getting game info class
-        var pubdata = info.find('[itemprop=datePublished]').text();
-        var size = info.find('[itemprop=fileSize]').text();
-        // var des = main.find('[jsname=C4s9Ed]').text();
-        // var p=  $('[jsname=C4s9Ed]')
-        // var des=p.find('p').text();
-        var des = [];
+        var wordOfDay = [],
+            main = $('.main-content'),
+            t = main.find('.document-title'),
+            info = main.find('.meta-info '),
+            title = t.find('.id-app-title').text(),
+            pubdata = info.find('[itemprop=datePublished]').text(),
+            size = info.find('[itemprop=fileSize]').text(),
+            ty,
+            des = [];
         $('p').each(function (i, elem) {
             des[i] = $(this).text();
-
         });
-        var ty;
+
         $('a.document-subtitle.category').filter(function () {
-
-            var data = $(this);
-            var href = data.attr('href');
-            ty = href.split('/');
-            console.log(ty[4])
-            cat = data.children().first().text();
-
-        })
-        // create an object
+                var data = $(this),
+                    href = data.attr('href');
+                ty = href.split('/');
+                console.log(ty[4])
+                cat = data.children().first().text();
+            })
+            /* create an object */
         wordOfDay.push({
             gameTitle: title,
             Gametype: cat
         })
-        // res.send(wordOfDay);
-        var gameDes = [];
-        var slp = ty[4].split('_');
-        var ty1 = slp[0];
-        var flag = false;
-        var final=[];
-        console.log(ty1)
+        var gameDes = [],
+            slp = ty[4].split('_'),
+            ty1 = slp[0],
+            flag = false,
+            final = [];
         if (ty1 == 'GAME') flag = true;
         var cato = 'Catageory=' + cat + "::" + "isGame=" + flag;
         db.hset([finalhkey, url, cato], redis.print);
         db.hgetAsync(finalhkey, url).then(function (data) {
-            final.push({Catageory:cat,gameStatus:cato})
+            final.push({
+                Catageory: cat,
+                gameStatus: cato
+            })
             res.send(final);
         })
-
     });
-    // });
 });
-// start app on localhost port 3000
-var port = process.env.PORT || 3006;
+
+/**
+ * listen port
+ */
 app.listen(port, function () {
     console.log('listening on port ' + port);
 });
